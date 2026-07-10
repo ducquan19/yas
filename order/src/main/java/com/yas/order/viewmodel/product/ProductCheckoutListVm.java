421ec9f5b92f19c0a0aaa21e0995d175f2eb709b
Tran Duc Quan <ducq317@gmail.com>
2026-07-11 03:56:00 +0700

    Fix Jackson deserialization error for ProductCheckoutListVm in order service

--- order/src/main/java/com/yas/order/viewmodel/product/ProductCheckoutListVm.java
+++ order/src/main/java/com/yas/order/viewmodel/product/ProductCheckoutListVm.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 
+@Jacksonized
 @Builder(toBuilder = true)
 @Data
 @NoArgsConstructor
