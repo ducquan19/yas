9d7f1177590aaa26e986227c3541f68f2c43d05d
Tran Duc Quan <ducq317@gmail.com>
2026-07-11 04:53:41 +0700

    chore: add a blank line for improved readability in SecurityConfig

--- order/src/main/java/com/yas/order/config/SecurityConfig.java
+++ order/src/main/java/com/yas/order/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 public class SecurityConfig {
     @Bean
